7ddf32fdecac3a6aeacbe44d7f842b238b640c54
Logan Byers <lbyers47@gmail.com>
2016-10-06 15:40:56 -0500

    working clear and new parameters

--- javascript.js
+++ javascript.js
@@ -1,15 +1,29 @@
 var x = 0;
+var z = 0;
 function sizeselect() {
-  var x = document.getElementById("size").value;
-    console.log(x);
-    for (i = 1; i <= (x*x); ++i){
-    $("#wrapper").append( $('<div/>')
-    .addClass("squares")
-    );
-      console.log("got here");
-      }
-    var y = x * 20 + "px";
-    $("#wrapper").removeAttr('style').css("width",y);
-    console.log(y);
-        
-    }
+     var x = document.getElementById("size").value;
+     $('.squares').remove();
+     $('.squarescolored').remove();
+     for (i = 1; i <= (x*x); ++i){
+        $("#wrapper").append( $('<div/>').addClass("squares"))
+        ;}
+     var y = x * 20 + "px";
+     $("#wrapper").removeAttr('style').css("width",y);
+     $(document).ready(function () {
+        $('.squares').hover(function () {
+        $(this).addClass('squarescolored');}, function(){
+        $(this).removeClass('squares');
+        });
+    });
+     
+ };
+    
+
+$(document).ready(function () {
+    $(function(){
+    $('#clear').click(function() {
+        $('.squarescolored').addClass('squares');
+        $('.squares').removeClass('squarescolored'); 
+    });
+});});
+    
